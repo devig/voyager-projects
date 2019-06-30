6a150daaca010baf9f01a3c8821852824679f6e4
Thomas Venturini <thomas@venturini.codes>
2019-06-30 16:14:39 +0200

    create configuration

---
+++ config/voyager-projects.php
@@ -0,0 +1,41 @@
+<?php
+        
+/*
+|--------------------------------------------------------------------------
+| Voyager Projects Configuration
+|--------------------------------------------------------------------------
+|
+| In this configuration file you will find all options for this package.
+|
+*/
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationships
+    |--------------------------------------------------------------------------
+    |
+    | In this section you will find all options for the relationships of this
+    | model.
+    |
+    */
+
+    'models' => [
+        // 'users' => \App\User::class,
+        // 'tags' => \App\Tag::class,
+        // 'posts' => App\Post::class,
+    ],
+   
+    'tables' => [
+        // 'users' => 'project_user',
+        // 'tags' => 'project_tag',
+    ],
+
+    'foreign_keys' => [
+        // 'projects' => 'project_id',
+        // 'project' => 'project_id',
+        // 'users' => 'user_id',
+        // 'tags' => 'tag_id',
+    ],
+];
