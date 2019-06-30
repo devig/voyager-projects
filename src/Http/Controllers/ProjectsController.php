55abaad44344d9c9139cfd5ba29b27746d59bb58
Thomas Venturini <thomas@venturini.codes>
2019-06-30 16:14:58 +0200

    create controller

---
+++ src/Http/Controllers/ProjectsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tjventurini\VoyagerProjects\Http\Controllers\Voyager;
+
+use Illuminate\Http\Request;
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
+
+class ProjectsController extends VoyagerBaseController
+{
+    /**
+     * General validation.
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    private function validation(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|min:3',
+            'description' => 'required|min:3',
+            'slug' => 'required|min:3',
+            // 'project_belongstomany_user_relationship' => 'required|exists:users,id',
+        ]);
+    }
+
+    /**
+     * POST BRE(A)D - Store data.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $this->validation($request);
+        return parent::store($request);
+    }
+
+    /**
+     * POST BR(E)AD - Update data.
+     * @param  \Illuminate\Http\Request $request
+     * @param  int  $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validation($request);
+        return parent::update($request, $id);
+    }
+}
