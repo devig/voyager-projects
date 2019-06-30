19a131b3ebd298af7a068534d733f7c0a661c72d
Thomas Venturini <thomas@venturini.codes>
2019-06-30 16:14:51 +0200

    create lang file

---
+++ resources/lang/en/projects.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Voyager Projects Translations
+|--------------------------------------------------------------------------
+|
+| In this file you will find all translations of this package.
+|
+*/
+
+return [
+    'label_plural' => 'Projects',
+    'label_singular' => 'Project',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Fields
+    |--------------------------------------------------------------------------
+    |
+    | In this section you will find all translations of fields in the admin
+    | panel.
+    |
+    */
+    
+    'data_rows' => [
+    ],
+    
+];
