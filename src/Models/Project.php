4114fb47880779ef4af18f310864f2dbf797ce88
Thomas Venturini <thomas@venturini.codes>
2020-01-05 03:45:27 +0100

    remove old relationships from model

--- src/Models/Project.php
+++ src/Models/Project.php
@@ -5,13 +5,11 @@ namespace Tjventurini\VoyagerProjects\Models;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Tjventurini\VoyagerProjects\Scopes\UsersScope;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
     use Sluggable;
-    
+
     protected $guarded = ['id'];
 
     /*
@@ -23,7 +21,7 @@ class Project extends Model
     | scopes.
     |
     */
-    
+
     /**
      * The "booting" method of this model.
      *
@@ -36,65 +34,6 @@ class Project extends Model
         // apply scopes
         static::addGlobalScope(new UsersScope);
     }
-    
-    /*
-    |--------------------------------------------------------------------------
-    | Relationships
-    |--------------------------------------------------------------------------
-    |
-    | In this section you will find all relationships of this model.
-    |
-    */
-    
-    /**
-     * Relationship with user model.
-     * @return Illuminate\Database\Eloquent\Relations\BelongsToMany The requested relationship.
-     */
-    public function users(): BelongsToMany
-    {
-        $model = config('voyager-projects.models.users');
-        $table = config('voyager-projects.tables.users');
-        $project_id = config('voyager-projects.foreign_keys.projects');
-        $user_id = config('voyager-projects.foreign_keys.users');
-
-        return $this->belongsToMany($model, $table, $project_id, $user_id);
-    }
-
-    /**
-     * Relationship with post model.
-     * @return Illuminate\Database\Eloquent\Relations\HasMany The requested relationship.
-     */
-    public function posts(): HasMany
-    {
-        $model = config('voyager-projects.models.posts');
-        $project_id = config('voyager-projects.foreign_keys.project');
-
-        return $this->hasMany($model, $project_id);
-    }
-
-    /**
-     * Relationship with page model.
-     * @return Illuminate\Database\Eloquent\Relations\HasMany The requested relationship.
-     */
-    public function pages(): HasMany
-    {
-        $model = config('voyager-projects.models.pages');
-        $project_id = config('voyager-projects.foreign_keys.projects');
-
-        return $this->hasMany($model, $project_id);
-    }
-
-    /**
-     * Relationship with contentBlock model.
-     * @return Illuminate\Database\Eloquent\Relations\HasMany The requested relationship.
-     */
-    public function contentBlocks(): HasMany
-    {
-        $model = config('voyager-projects.models.content-blocks');
-        $project_id = config('voyager-projects.foreign_keys.projects');
-
-        return $this->hasMany($model, $project_id);
-    }
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +43,7 @@ class Project extends Model
     | In this section you will find the settings for the Sluggable Trait.
     |
     */
-    
+
     /**
      * Return the sluggable configuration array for this model.
      *
@@ -114,8 +53,8 @@ class Project extends Model
     {
         return [
             'slug' => [
-                'source' => 'name'
-            ]
+                'source' => 'name',
+            ],
         ];
     }
 }
